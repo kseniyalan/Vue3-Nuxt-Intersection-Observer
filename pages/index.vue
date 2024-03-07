fe7eeb668571c91034e2194d74df03fcfe95d127
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-03-07 10:11:10 +0100

    Fix site colors

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@ onMounted(() => {
       <TextBlock>
         <template #text-top><span class="orange">Let's start!</span></template>
         <template #text-main>
-          The Intersection Observer API provides a way to <span class="purple-light">asynchronously observe</span> changes in the intersection of a target element with an ancestor element or with a top-level document's viewport.
+          The Intersection Observer API provides a way to <span class="red">asynchronously observe</span> changes in the intersection of a target element with an ancestor element or with a top-level document's viewport.
         </template>
       </TextBlock>
     </div>
@@ -54,7 +54,7 @@ onMounted(() => {
 
     <div class="fade-in-delay-wrapper" :ref="el => sectionRefs.push(el)">
       <TextBlock>
-        <template #text-top><span class="yellow">Next!</span></template>
+        <template #text-top><span class="green">Next!</span></template>
         <template #text-main>
           Historically, detecting visibility of an element, or the relative visibility of two elements in relation to each other, has been a difficult task for which solutions have been unreliable and prone to causing the browser and the sites the user is accessing to become sluggish.
         </template>
@@ -69,17 +69,17 @@ onMounted(() => {
 
       <!-- Slide left section -->
       <div class="col-right col-12 col-md-6 d-flex align-items-start align-items-md-center ps-0 ps-md-2 pt-4 pt-md-0" :ref="el => sectionRefs.push(el)">
-        <div class="site-text-regular">
+        <div class="site-text-regular large">
           Implementing intersection detection in the past involved event handlers and loops calling methods like <span class="purple"><i>Element.getBoundingClientRect()</i></span> to build up the needed information for every element affected. Since all this code runs on the main thread, even one of these can cause performance problems. When a site is loaded with these tests, things can get downright ugly.
         </div>
       </div>
     </div>
 
-    <div class="col-12 col-md-7 site-text-regular py-4 py-md-6 mx-auto">
-      <div class="mb-2 mb-md-4">As the web has matured, the need for this kind of information has grown. Intersection information is needed for many reasons, such as: </div>
-        <ul class="site-ul">
+    <div class="col-12 col-md-7 py-4 py-md-6 mx-auto">
+      <div class="site-text-regular large mb-2 mb-md-4">As the web has matured, the need for this kind of information has grown. Intersection information is needed for many reasons, such as: </div>
+        <ul class="site-text-regular medium site-ul">
           <li>Lazy-loading of images or other content as a page is scrolled.</li>
-          <li>Implementing <span class="yellow">"infinite scrolling"</span> websites, where more and more content is loaded and rendered as you scroll, so that the user doesn't have to flip through pages.</li>
+          <li>Implementing <span class="green">"infinite scrolling"</span> websites, where more and more content is loaded and rendered as you scroll, so that the user doesn't have to flip through pages.</li>
           <li>Reporting of visibility of advertisements in order to calculate ad revenues.</li>
           <li>Deciding whether or not to perform tasks or animation processes based on whether or not the user will see the result.</li>
         </ul> 
